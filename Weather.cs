8e83cb6dfcd1036d69abdec5a9cae0aae812ec42
Charles Sather <charliesather18@gmail.com>
2020-01-16 11:42:46 -0600

    Added methods to help create random weather condtions

--- Weather.cs
+++ Weather.cs
@@ -13,13 +13,76 @@ namespace LemondeStandProject
         List<string> weatherConditions;
         public string predictedForecast { get; set; }
 
-        public Weather(string condition, int temperature, List<string> weatherConditions, string predictedForecast)
+        public Weather(string condition, int temperature, string predictedForecast)
         {
             this.condition = condition;
             this.temperature = temperature;
-            this.weatherConditions = weatherConditions;
             this.predictedForecast = predictedForecast;
+
+            weatherConditions = new List<string>
+            {
+                "Sunny",
+                "Cloudy",
+                "Rainy",
+                "Stormy",
+                "Snowing",
+                "Windy",
+                "Icy",
+            };
         }
+        public Weather()
+        {
+
+        }
+
+        public void SetRandomCondition()
+        {
+            Random rand = new Random();
+
+            condition = weatherConditions[rand.Next(0, weatherConditions.Count)]; 
+        }
+
+        public void SetTemperature(int temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public void SetRandomTemperature()
+        {
+            Random rand = new Random();
+            switch (condition)
+            {
+                case ("Sunny"):
+                    temperature = rand.Next(70, 81);
+                    break;
+                case ("Cloudy"):
+                    temperature = rand.Next(60, 71);
+                    break;
+                case ("Rainy"):
+                    temperature = rand.Next(40, 61);
+                    break;
+                case ("Stormy"):
+                    temperature = rand.Next(45, 66);
+                    break;
+                case ("Snowing"):
+                    temperature = rand.Next(0, 33);
+                    break;
+                case ("Windy"):
+                    temperature = rand.Next(70, 81);
+                    break;
+                case ("Icy"):
+                    temperature = rand.Next(-10, 21);
+                    break;
+                default:
+                    Console.WriteLine("Couldn't set temp");
+                    break;
+            }
+
+        }
+
+        
+
+
 
 
 
